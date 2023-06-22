11c525487e6aa9067f177bead09805a81fb9ea0f
Evgeniya <j.nikif15@gmail.com>
2023-06-22 22:02:48 +0300

    homework for the seminar5

---
+++ HW_5/task_1.py
@@ -0,0 +1,12 @@
+# Напишите функцию, которая принимает на вход строку -
+# абсолютный путь до файла. Функция возвращает кортеж из трёх элементов:
+# путь, имя файла, расширение файла.
+
+def path_elem(text):
+    way = '\\'.join(text.split('\\')[:-1])
+    *_, name, expansion = text.replace('.', '\\').split('\\')
+    result = (way, name, expansion)
+    return result
+
+
+print(path_elem("C:\ProgramFiles\HW_5\\task_1.py"))
